4afcb5c38250d57ac6e3df47af864875e2309eba
KimiruMaekumi <kimiru.maekumi@gmail.com>
2022-02-18 13:17:52 +0200

    Reading the information and plotting the waveform of a .wav file

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # !Change file path of the .wav file
+    audio_file_path = 'C:\\Users\\User\\Desktop\\well_16k.wav'  # if file contains INFO chunks (created by editing
+    # software) then a WavFileWarning is thrown
+
+    # Read the data and calculate information about the audio file
+    sample_rate = wavfile.read(audio_file_path)[0]
+    audio_data = wavfile.read(audio_file_path)[1]
+    number_of_channels = audio_data.shape[1]    # dimensions of the audio file
+    length = audio_data.shape[0] / sample_rate    # time in seconds (total samples / sample rate)
+
+    # Print information about the audio data
+    print(f"file = \t{audio_file_path}")
+    print(f"sample rate = \t{sample_rate}")
+    print(f"channels = \t{number_of_channels}")
+    print(f"length = \t{length} sec")
+
+    # Show a plot of the waveform
+    time = np.linspace(0., length, audio_data.shape[0])
+    plt.plot(time, audio_data[:, 0], label="Left channel")
+    plt.plot(time, audio_data[:, 1], label="Right channel")
+    plt.legend()
+    plt.xlabel("Time [s]")
+    plt.ylabel("Amplitude")
+    plt.show()
